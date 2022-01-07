215f52fd63ec2de01482f6d6bca860b7480ee5a4
shk3029 <shk30290@naver.com>
2022-01-07 22:44:36 +0900

    Initial commit

---
+++ src/main/java/com/example/studyclouduserservice/StudyCloudUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.studyclouduserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyCloudUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyCloudUserServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studyclouduserservice/StudyCloudUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studyclouduserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyCloudUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
