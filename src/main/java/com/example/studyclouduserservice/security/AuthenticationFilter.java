97f572f842374614411f19024c1685c92fb5b427
shk3029 <shk30290@naver.com>
2022-02-09 22:48:36 +0900

    feat: 로그인 처리 과정 & 로그인 성공 처리

--- src/main/java/com/example/studyclouduserservice/security/AuthenticationFilter.java
+++ src/main/java/com/example/studyclouduserservice/security/AuthenticationFilter.java
@@ -1,11 +1,17 @@
 package com.example.studyclouduserservice.security;
 
 
+import com.example.studyclouduserservice.dto.UserDto;
+import com.example.studyclouduserservice.service.UserService;
 import com.example.studyclouduserservice.vo.RequestLogin;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.FilterChain;
@@ -15,7 +21,17 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
 
+@Slf4j
 public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final UserService userService;
+    private final Environment env;
+
+    public AuthenticationFilter(AuthenticationManager authenticationManager, UserService userService, Environment env) {
+        super(authenticationManager);
+        this.userService = userService;
+        this.env = env;
+    }
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request,
@@ -41,5 +57,7 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
                                             HttpServletResponse response,
                                             FilterChain chain,
                                             Authentication authResult) throws IOException, ServletException {
+        String userName = ((User)authResult.getPrincipal()).getUsername();
+        UserDto userDetails = userService.getUserDetailsByEmail(userName);
     }
 }
